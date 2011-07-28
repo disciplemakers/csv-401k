ec2ea9e25d558b4f65ccbe1fc614cf5bf3f4743f
Brian Roberg and Katrina Layman <brianandkatrina@dm.org>
2011-07-28 11:14:49 -0400

    Initial creation of Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csv401k::Application.config.secret_token = 'd4f1d7b651776f4dbabaad540729a09c6057407bf2e693ea41f7e040ba55f786d560b0b2a6d4263b26e3bb1c56397b23217053f011032e753843d86c73985634'
